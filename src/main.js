61bc2a15017228aba6c2fa68bf50d4f1c7db9b22
andreresende36 <andreresende36@gmail.com>
2022-11-16 17:37:58 -0300

    Resolução do requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productsSection = document.getElementsByClassName('products');
+const searchComputador = await fetchProductsList('computador');
+
+searchComputador.forEach((element, i) => {
+  const { id, title, thumbnail, price } = element;
+  const productElement = createProductElement({ id, title, thumbnail, price });
+  productsSection[0].appendChild(productElement);
+});
