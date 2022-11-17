e4653fecbf653e02aa81e89ff3bab709e6c65380
andreresende36 <andreresende36@gmail.com>
2022-11-16 21:37:10 -0300

    Resolução do requisito 5

--- src/main.js
+++ src/main.js
@@ -7,27 +7,37 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const productsSection = document.getElementsByClassName('products');
 
-const loading = () => {
-  const loadingDisplay = document.createElement('span');
-  loadingDisplay.className = 'loading';
-  loadingDisplay.innerHTML = 'carregando...';
-  productsSection[0].appendChild(loadingDisplay);
-};
-
-const disableLoading = () => {
-  const loadingDisplay = document.querySelector('.loading');
-  loadingDisplay.remove();
+const loading = (bool) => {
+  if (bool === true) {
+    const loadingDisplay = document.createElement('span');
+    loadingDisplay.className = 'loading';
+    loadingDisplay.innerHTML = 'carregando...';
+    productsSection[0].appendChild(loadingDisplay);
+  } else if (bool === false) {
+    const loadingDisplay = document.querySelector('.loading');
+    loadingDisplay.remove();
+  } else if (bool === undefined) {
+    const error = document.createElement('span');
+    error.className = 'error';
+    error.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    loading(false);
+    productsSection[0].appendChild(error);
+  }
 };
 
 const fillProductsList = async (query) => {
-  loading();
-  const search = await fetchProductsList(query);
-  disableLoading();
-  search.forEach((element) => {
-    const { id, title, thumbnail, price } = element;
-    const productElement = createProductElement({ id, title, thumbnail, price });
-    productsSection[0].appendChild(productElement);
-  });
+  loading(true);
+  try {
+    const search = await fetchProductsList(query);
+    loading(false);
+    search.forEach((element) => {
+      const { id, title, thumbnail, price } = element;
+      const productElement = createProductElement({ id, title, thumbnail, price });
+      productsSection[0].appendChild(productElement);
+    });
+  } catch (e) {
+    loading();
+  }
 };
 
 fillProductsList('computador');
