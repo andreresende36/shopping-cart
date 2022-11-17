aaea0c43a6d6ddca8d3b9cf5f5000893ea2923d4
andreresende36 <andreresende36@gmail.com>
2022-11-16 21:18:10 -0300

    Resolução do requisito 4

--- src/main.js
+++ src/main.js
@@ -7,8 +7,22 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const productsSection = document.getElementsByClassName('products');
 
+const loading = () => {
+  const loadingDisplay = document.createElement('span');
+  loadingDisplay.className = 'loading';
+  loadingDisplay.innerHTML = 'carregando...';
+  productsSection[0].appendChild(loadingDisplay);
+};
+
+const disableLoading = () => {
+  const loadingDisplay = document.querySelector('.loading');
+  loadingDisplay.remove();
+};
+
 const fillProductsList = async (query) => {
+  loading();
   const search = await fetchProductsList(query);
+  disableLoading();
   search.forEach((element) => {
     const { id, title, thumbnail, price } = element;
     const productElement = createProductElement({ id, title, thumbnail, price });
