3c023b69c346961c47a26c77190f38369c0818e4
andreresende36 <andreresende36@gmail.com>
2022-11-17 07:53:29 -0300

    Resolução do requisito 8

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
+import { saveCartID } from './helpers/cartFunctions';
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const productsSection = document.getElementsByClassName('products');
+const cartProducts = document.querySelector('.cart__products');
 
 const loading = (bool) => {
   if (bool === true) {
@@ -25,6 +27,14 @@ const loading = (bool) => {
   }
 };
 
+const cartButtonFn = async (event) => {
+  const productBox = event.target.parentElement;
+  const id = productBox.querySelector('.product__id').innerText;
+  saveCartID(id);
+  const product = await fetchProduct(id);
+  cartProducts.appendChild(createCartProductElement(product));
+};
+
 const fillProductsList = async (query) => {
   loading(true);
   try {
@@ -33,6 +43,8 @@ const fillProductsList = async (query) => {
     search.forEach((element) => {
       const { id, title, thumbnail, price } = element;
       const productElement = createProductElement({ id, title, thumbnail, price });
+      const cartButton = productElement.querySelector('.product__add');
+      cartButton.addEventListener('click', cartButtonFn);
       productsSection[0].appendChild(productElement);
     });
   } catch (e) {
